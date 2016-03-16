e5fe0b8889cfb78b4ac16d4a212ae7b896be5dd5
Beramos <bram.de.jaegher@gmail.com>
2016-03-16 14:54:43 +0100

    proper implementation of non parametric identification

---
+++ Project1/Corr.m
@@ -0,0 +1,56 @@
+% Matlab-script for non-parametric identification using correlation
+% functions
+
+clear all; clc; close all;
+
+% Importing data
+data=importdata('IDENTIFICATION_PRESSURE_Save_05.txt');
+u=data(:,1);      % first column is PRBS input signal
+y=data(:,2);      % second column is output signal
+
+% Parameters
+
+N=size(data,1);    % number of samples
+Be=0.02;           % frequency resolution  (Hz)
+M1=N;              % Sample interval length  M=N/p for p=1
+M2=floor(N/4);     % Sample interval length for p=4 (integers) 
+
+% Bodeplot
+
+Suu_M1=spectra(u,u,M1);
+Suy_M1=spectra(u,y,M1);
+
+% initialisation of Suu and Suy + calculation of the first M-bin
+Suu_M2=spectra(u(1:M2),u(1:M2),M2); 
+Suy_M2=spectra(u(1:M2),y(1:M2),M2);
+
+for i=2:4
+sample_indices=1+(i-1)*M2:i*M2;
+Suu_M2=Suu_M2+spectra(u(sample_indices),u(sample_indices),M2);  
+Suy_M2=Suy_M2+spectra(u(sample_indices),y(sample_indices),M2);
+end
+
+Suu_M2=Suu_M2/4;                       % averaging of the spectra
+Suy_M2=Suy_M2/4;                       % averaging of the spectra
+
+Magnitude_M1=sqrt(real(Suy_M1).^2+imag(Suy_M1).^2)./abs(real(Suu_M1));
+Phase_M1=360/(2*pi).*(atan(imag(Suy_M1)./real(Suy_M1)));
+f_M1=2*pi*Be*linspace(0,N-1,length(Suu_M1));
+
+Magnitude_M2=sqrt(real(Suy_M2).^2+imag(Suy_M2).^2)./abs(real(Suu_M2));
+Phase_M2=360/(2*pi).*(atan(imag(Suy_M2)./real(Suy_M2)));
+f_M2=2*pi*Be*linspace(0,N-1,length(Suu_M2));
+
+figure(1);
+subplot(2,1,1)
+semilogx(f_M1,20*log10(Magnitude_M1),'b',f_M2,20*log10(Magnitude_M2),'r');
+xlabel('Frequency (rad/s)')
+ylabel('Magnitude (dB)')
+legend('M=500','M=125')
+
+subplot(2,1,2)
+semilogx(f_M1,Phase_M1,'b',f_M2,Phase_M2,'r')
+xlabel('Frequency (rad/s)')
+ylabel('Phase (degrees)')
+legend('M=500','M=125')
+
---
+++ Project1/spectra.m
@@ -0,0 +1,31 @@
+function [S]=spectra(A,B,M)
+% SPECTRA calculates the spectra
+
+
+% Take out mean value
+u=A-mean(A);
+y=B-mean(B);
+
+% calculate correlation using xcorr
+R=xcorr(u,y,M,'unbiased');  
+
+% Take middle part of correlation vector
+
+R_cut=R(round(M/2):length(R)-round(M/2),1);
+
+% Window with Blackman function
+
+window=blackman(length(R_cut));
+R_w=transpose(R_cut.*window);
+
+% Swapping
+
+l=length(R_w);
+R_s=[R_w(M:end) R_w(1:M-1)];
+
+
+% Calculate spectrum using Fast Fourier Transform
+
+S=fft(R_s);
+
+end
