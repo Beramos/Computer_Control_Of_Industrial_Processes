47caa53435c8d1038b672863f96c2bcf1700e7a8
Beramos <bram.de.jaegher@gmail.com>
2016-04-02 07:57:05 +0200

    added project 3

--- Project1/Non-parametric/plotValidation_identif.m
+++ Project1/Non-parametric/plotValidation_identif.m
@@ -1,7 +1,7 @@
 clear all; clc; close all;
 
 % Importing data
-data=importdata('IDENTIFICATION_PRESSURE_Save_05.txt');
+data=importdata('IDENTIFICATION_PRESSURE_Save_01.txt');
 u=data(:,1);      % first column is PRBS input signal
 y=data(:,2);      % second column is output signal
 Ts=0.1;
@@ -17,7 +17,8 @@ Ts=0.1;
  y=y(1:200);
  
 figure; % plot data
-subplot(2,1,1); hold on;  plot((0:199)*Ts,u,'k'); plot((200:400)*Ts,uValidation,'r'); hold off;
+subplot(2,1,1); hold on;  plot((0:199)*Ts,u); plot((200:400)*Ts,uValidation,'r'); hold off;
 xlabel('time (s)'); ylabel('input signal (V)')
-subplot(2,1,2); hold on;  plot((0:199)*Ts,y,'k'); plot((200:400)*Ts,yValidation,'r'); hold off;
+ylim([6 8])
+subplot(2,1,2); hold on;  plot((0:199)*Ts,y); plot((200:400)*Ts,yValidation,'r'); hold off;
 xlabel('time (s)'); ylabel('output signal (mbar)')
---
+++ Project3/stepResponse.m
@@ -0,0 +1,28 @@
+% This script plots the step responses of the MIMO system to a [0;1] and
+% [1;0] input step.
+
+close all; clear all; clc;
+
+simulationTime=800;
+
+% [1;0] step response
+u1_fin=1;
+u2_fin=0;
+
+sim('stepResponseModel')
+
+figure; plot(t,y1,t,y2)
+legend('y1','y2')
+xlabel('time [s]')
+ylabel('tank level [cm]')
+
+% [0;1] step response
+u1_fin=0;
+u2_fin=1;
+
+sim('stepResponseModel')
+
+figure; plot(t,y1,t,y2)
+legend('y1','y2')
+xlabel('time [s]')
+ylabel('tank level [cm]')
